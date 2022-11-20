62005f403a1a6760e1cf6b970ffe4a2fb382ada3
Alexey Tagarov <tagarov@gmail.com>
2022-11-20 16:23:18 +0700

    открытие закрытие попапов карточек в классе Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,8 +1,24 @@
+const popupElement = document.querySelector(".popup_type_card");
+const popupElementImage = popupElement.querySelector(".popup-figure__image");
+const popupElementCaption = popupElement.querySelector(
+  ".popup-figure__caption"
+);
+const popupElementCloseButton = popupElement.querySelector(
+  ".popup__close-button"
+);
+
 class Card {
   constructor(data) {
     this._name = data.name;
     this._link = data.link;
   }
+
+  _handleEscKeyToClosePopup = (evt) => {
+    if (evt.key === "Escape") {
+      this._handleClosePopup();
+    }
+  };
+
   _getTemplate() {
     // забираем разметку из HTML и клонируем элемент
     const cardElement = document
@@ -12,6 +28,27 @@ class Card {
     // вернём DOM-элемент карточки
     return cardElement;
   }
+
+  _handleOpenPopup() {
+    popupElementImage.setAttribute("src", this._link);
+    popupElementImage.setAttribute("alt", `Фото ${this._name}`);
+    popupElementCaption.textContent = this._name;
+    popupElement.classList.add("popup_opened");
+    document.addEventListener("keydown", this._handleEscKeyToClosePopup);
+  }
+
+  _handleClosePopup() {
+    popupElement.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._handleEscKeyToClosePopup);
+  }
+
+  _setEventListeners() {
+    this._element.addEventListener("click", () => this._handleOpenPopup());
+    popupElementCloseButton.addEventListener("click", () =>
+      this._handleClosePopup()
+    );
+  }
+
   generateCard() {
     //Получаем DOM-элемент карточки
     this._element = this._getTemplate();
@@ -24,6 +61,7 @@ class Card {
     );
     elementImage.setAttribute("src", this._link);
     elementImage.setAttribute("alt", `Фото ${this._name}`);
+    this._setEventListeners();
 
     return this._element;
   }
